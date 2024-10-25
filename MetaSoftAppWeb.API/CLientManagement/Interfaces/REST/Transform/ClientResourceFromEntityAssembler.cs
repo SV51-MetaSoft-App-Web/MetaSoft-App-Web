b143dc781cb239db76146a3787e5405a6a2829e6
petitavo <Gustavohuanca1020@gmail.com>
2024-10-25 13:06:13 -0500

    feat(client-management): Client entity added.

---
+++ MetaSoftAppWeb.API/CLientManagement/Interfaces/REST/Transform/ClientResourceFromEntityAssembler.cs
@@ -0,0 +1,22 @@
+﻿using MetaSoftAppWeb.API.ClientManagement.Domain.Model.Commands;
+using MetaSoftAppWeb.API.ClientManagement.Interfaces.REST.Resources;
+
+namespace MetaSoftAppWeb.API.ClientManagement.Interfaces.REST.Transform;
+
+/// <summary>
+/// Assembler to create a client resource from an entity
+/// </summary>
+public static class ClientResourceFromEntityAssembler
+{
+    /// <summary>
+    /// Converts a <see cref="Clients"/> entity to a <see cref="ClientResource"/>
+    /// </summary>
+    /// <param name="entity">The <see cref="Clients"/> entity</param>
+    /// <returns>
+    /// A <see cref="ClientResource"/> resource
+    /// </returns>
+    public static ClientResource ToResourceFromEntity(Clients entity)
+    {
+        return new ClientResource(entity.Id,entity.PersonName, entity.Dni, entity.Email, entity.BusinessName, entity.Phone, entity.Address, entity.Country, entity.City, entity.Ruc);
+    }
+}       
