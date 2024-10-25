c645d9d4a3618a5176eb22455b60772a6692ec1b
petitavo <Gustavohuanca1020@gmail.com>
2024-10-25 13:05:23 -0500

    feat(client-management): Client Query Service added.

---
+++ MetaSoftAppWeb.API/CLientManagement/Application/Internal/QueryServices/ClientQueryService.cs
@@ -0,0 +1,18 @@
+﻿
+using MetaSoftAppWeb.API.ClientManagement.Domain.Model.Commands;
+using MetaSoftAppWeb.API.ClientManagement.Domain.Model.Queries;
+using MetaSoftAppWeb.API.ClientManagement.Domain.Repositories;
+using MetaSoftAppWeb.API.ClientManagement.Domain.Services;
+
+/// <summary>
+/// Query service for clients
+/// </summary>
+/// <param name="clientRepository">ClientRepository instance</param>
+public class ClientQueryService(IClientRepository clientRepository) : IClientQueryService
+{
+    /// <inheritdoc cref="IClientQueryService.Handle(GetClientByIdQuery)"/>
+    public async Task<Clients?> Handle(GetClientByIdQuery query)
+    {
+        return await clientRepository.FindByIdAsync(query.Id);
+    }
+}
