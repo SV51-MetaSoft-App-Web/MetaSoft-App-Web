da929faefe6363f455ef023db284e3148d538d69
petitavo <Gustavohuanca1020@gmail.com>
2024-10-25 13:05:39 -0500

    feat(client-management): Client repository added.

---
+++ MetaSoftAppWeb.API/CLientManagement/Infrastructure/Persistence/EFC/Repositories/ClientRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using MetaSoftAppWeb.API.ClientManagement.Domain.Model.Commands;
+using MetaSoftAppWeb.API.ClientManagement.Domain.Repositories;
+using MetaSoftAppWeb.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+using MetaSoftAppWeb.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace MetaSoftAppWeb.API.ClientManagement.Infrastructure.Persistence.EFC.Repositories;
+
+public class ClientRepository(AppDbContext context)
+: BaseRepository<Clients>(context), IClientRepository
+{
+    
+}
