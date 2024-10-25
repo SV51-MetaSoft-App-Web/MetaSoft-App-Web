f7a3f3f07ca9a0f5c1d71fbb6fece0d013416aa3
petitavo <Gustavohuanca1020@gmail.com>
2024-10-25 13:03:51 -0500

    feat(orm): Db Context added.

--- MetaSoftAppWeb.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ MetaSoftAppWeb.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,9 +1,9 @@
-using MetaSoftAppWeb.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
-using MetaSoftAppWeb.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions.Extensions;
+using MetaSoftAppWeb.API.ClientManagement.Domain.Model.Commands;
+using MetaSoftAppWeb.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions.Extensions;
 using Microsoft.EntityFrameworkCore;
 
-namespace MetaSoftAppWeb.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
+namespace MetaSoftAppWeb.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
@@ -16,7 +16,19 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
-        
+        builder.Entity<Clients>().ToTable("Clients");
+        builder.Entity<Clients>().HasKey(f => f.Id);
+        builder.Entity<Clients>().Property(f => f.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Clients>().Property(f => f.PersonName).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.Dni).IsRequired().HasMaxLength(20);
+        builder.Entity<Clients>().Property(f => f.Email).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.BusinessName).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.Phone).IsRequired().HasMaxLength(20);
+        builder.Entity<Clients>().Property(f => f.Address).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.Country).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.City).IsRequired().HasMaxLength(100);
+        builder.Entity<Clients>().Property(f => f.Ruc).IsRequired().HasMaxLength(20);
+
         builder.UseSnakeCaseNamingConvention();
     }
 }
